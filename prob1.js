9e87e7bacec217d8fd48d69ffdb5049d7018c7d6
Mike Lee <mikelee131@gmail.com>
2015-09-12 22:08:32 -0700

    Finish and cleanup

--- prob1.js
+++ prob1.js
@@ -1,40 +1,69 @@
 /**
  * Problem 1
- * Given a set of n points (x,y), write an algorithm to pick the k closest to the origin (0,0)
+ * Given a set of n points (x,y), write an algorithm to 
+ * pick the k^th closest to the origin (0,0)
  *
  * Notes:
  * We need to know the distance between each point and the origin 
  * sqrt((x_2 - x_1)^2 + (y_2 - y_1)^2)
+ *
+ * Assumptions:
+ * Input will be an array of objects 
  */
 
-// Global namespace
-var testCoordinates = []
-var global = {};
+var print = console.log;
 
-global.loadTestData = function () {
-  testCoordinates.push(new coordinate(10, 1));
-  testCoordinates.push(new coordinate(8, 6));
-  testCoordinates.push(new coordinate(4, 9));
-  // testCoordinates.push(new coordinate(10, 1));
-};
+var points = [
+  {x: 1, y: 3},
+  {x: 3, y: 1},
+  {x: 4, y: 3},
+  {x: 2, y: 10},
+  {x: -10, y: 3},
+  {x: -1, y: 12}
+];
+var k = 3; // Third closest to origin
 
-global.greet = function () {
-  console.log(';wefhwohf');
-};
+var sortedCoordinates = [];
 
-global.sort = function () {
-
-};
-
-function coordinate(x, y) {
+function Coord (x, y) {
   this.x = x;
   this.y = y;
-  this.distanceFromOrigin = Math.sqrt( (this.x * this.x) + (this.y * this.y) );
-};
+  this.distanceFromOrigin = Math.sqrt((x * x) + (y * y));
+}
 
-(function (g, testData) {
-  var print = console.log;
-  // Fake main to call the solution function
-  g.loadTestData();
-  print(testCoordinates);
-})(global, testCoordinates);
+function insertSort (coord) {
+  if (sortedCoordinates.length === 0) {
+    sortedCoordinates.push(coord);
+    return;
+  }
+
+  for (var i = 0; i < sortedCoordinates.length; i++ ) {
+    if (coord.distanceFromOrigin === sortedCoordinates[i].distanceFromOrigin) {
+      sortedCoordinates.splice(i + 1, 0, coord);
+      break;
+    } else if (coord.distanceFromOrigin > sortedCoordinates[i].distanceFromOrigin) {
+      var tempCount = i;
+      while (tempCount < sortedCoordinates.length &&  coord.distanceFromOrigin > sortedCoordinates[tempCount].distanceFromOrigin) {
+        tempCount++;
+      }
+      sortedCoordinates.splice(tempCount, 0, coord);
+      break;
+    } else if (coord.distanceFromOrigin < sortedCoordinates[i].distanceFromOrigin) {
+      sortedCoordinates.splice(i, 0, coord);
+      break;
+    }
+  }
+}
+
+// Read in data
+for (var i = 0; i < points.length; i++) {
+  insertSort(new Coord(points[i].x, points[i].y));
+}
+
+// Get the kth closest
+print('The ', k, 'th from the origin is ', sortedCoordinates[k - 1]);
+
+// Print sorted
+for (coord in sortedCoordinates) {
+  print(coord, ' : (', sortedCoordinates[coord].x, ',', sortedCoordinates[coord].y, ') --> ', sortedCoordinates[coord].distanceFromOrigin);
+}
